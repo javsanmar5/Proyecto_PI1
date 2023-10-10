0efee93978a38f90a9dad6d795b4ed7416949b1a
javsanmar5 <javsanmar5@alum.us.es>
2023-10-10 21:59:01 +0200

    Cosas ejercicio2 laptop

--- src/main/Ejercicio2.java
+++ src/main/Ejercicio2.java
@@ -5,22 +5,46 @@ import java.util.List;
 
 public class Ejercicio2 {
 	
-	public static List<Integer> ejercicio2RecFinal(int a, int b){
-		
+	private static Boolean esCasoBase(int a, int b) {
+		return a < 2 || b < 2;
+	}
+	
+	//RECURSIVO NO FINAL
+	public static List<Integer> ejercicio2RecursivoNoF(int a, int b) {
+	
 		List<Integer> res = new ArrayList<>();
 		
-		if (a < 2 || b < 2) {
-			res.add(a*b);
-		} else if (a > b) {
+		if(esCasoBase(a, b)) {
+			res.add(a*b);	
+		}else if (a > b) {
 			res.add(a);
-			res.addAll(ejercicio2RecFinal(a%b, b-1));
+			res.addAll(ejercicio2RecursivoNoF(a%b, b-1));
 		} else {
 			res.add(b);
-			res.addAll(ejercicio2RecFinal(a-2, b/2));
+			res.addAll(ejercicio2RecursivoNoF(a-2, b / 2));
 		}
 		
 		return res;
-			
 	}
-
+	
+	
+	//RECURSIVO FINAL
+	public static List<Integer> ejercicio2RecursivoF(int a, int b) {
+		List<Integer> acum = new ArrayList<>();
+		return ejercicio2RecursivoF(a, b, acum);
+	}
+	
+	private static List<Integer> ejercicio2RecursivoF(int a, int b, List<Integer> acum){
+		if(esCasoBase(a,b)) {
+			acum.add(a*b);
+		} else if(a>b) {
+			acum.add(a);
+			acum = (ejercicio2RecursivoF(a%b, b/2, acum));
+		} else {
+			acum.add(b);
+			acum = (ejercicio2RecursivoF(a-2, b/2, acum));
+		}
+		return acum;
+	}
+	
 }
--- src/test/TestEjercicio2.java
+++ src/test/TestEjercicio2.java
@@ -15,17 +15,27 @@ public class TestEjercicio2 {
 	
 	private static void toPrint() {
 		
-		//PRUEBAS DEL EJERCICIO 2:
-		//Código recursivoFinal:
-		List<Integer> resEjercicio2RecFinal1 = Ejercicio2.ejercicio2RecFinal(5, 8);
-		List<Integer> resEjercicio2RecFinal2 = Ejercicio2.ejercicio2RecFinal(2, 7);
-		List<Integer> resEjercicio2RecFinal3 = Ejercicio2.ejercicio2RecFinal(9, 19);
+System.out.println("Ejercicio resuelto de forma recursiva no final:");
 		
-		System.out.println("Ejercicios resueltos con el método recursivo final;");
-		System.out.println(resEjercicio2RecFinal1);
-		System.out.println(resEjercicio2RecFinal2);
-		System.out.println(resEjercicio2RecFinal3);
+		List<Integer> ejercicio2RecursivoNoF1 = Ejercicio2.ejercicio2RecursivoNoF(5,8);
+		List<Integer> ejercicio2RecursivoNoF2 = Ejercicio2.ejercicio2RecursivoNoF(13,9);
+		List<Integer> ejercicio2RecursivoNoF3 = Ejercicio2.ejercicio2RecursivoNoF(15,26);
 		
+		System.out.println(ejercicio2RecursivoNoF1);
+		System.out.println(ejercicio2RecursivoNoF2);
+		System.out.println(ejercicio2RecursivoNoF3);
+		
+		
+		//Ejercicio 2 recursivo final.
+		System.out.println("\nEjercicio resuelto de forma recursiva final:");
+		
+		List<Integer> ejercicio2RecursivoF1 = Ejercicio2.ejercicio2RecursivoF(5,8);
+		List<Integer> ejercicio2RecursivoF2 = Ejercicio2.ejercicio2RecursivoF(13,9);
+		List<Integer> ejercicio2RecursivoF3 = Ejercicio2.ejercicio2RecursivoF(15,26);
+
+		System.out.println(ejercicio2RecursivoF1);
+		System.out.println(ejercicio2RecursivoF2);
+		System.out.println(ejercicio2RecursivoF3);
 		
 	}
 
